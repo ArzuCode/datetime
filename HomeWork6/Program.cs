fa2fed22198d252c0b1c2acde1b18266f4492b53
Arzu.Code <arzuem@code.edu.az>
2023-06-17 03:17:36 +0300

    first commit

---
+++ HomeWork6/Program.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualBasic;
+using System.Globalization;
+
+namespace HomeWork6
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string dateInput = "07/10/1998";
+            Console.WriteLine(IsValidDate(dateInput));
+        }
+
+        public static bool IsValidDate(string date)
+        {
+            DateTime tempObject;
+
+            return DateTime.TryParseExact(date, "dd/mm/yyyy", CultureInfo.InvariantCulture, DateTimeStyles.None, out tempObject);
+        }
+    }
+}
